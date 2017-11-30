965e92c96486b0956a3778cb33afc82025829285
邓道成 <dengdaocheng@xinhehui.com>
2017-11-30 19:29:41 +0800

    booksonline

--- index.php
+++ index.php
@@ -78,10 +78,7 @@ if($search_type == 'online') {
 		$redis->auth('dc0623');
 		$redis->select(2);
 
-		if (empty($q) || !$ret) {
-			throw_exception('请输入搜索关键词');
-		}
-		else {
+		if ($ret && $q) {
 			$redis->lPush('BOOK_SEARCH_QUEUE', $q);
 		}
 	}
@@ -90,105 +87,109 @@ if($search_type == 'online') {
 		die($error);
 	}
 }
-else {
-	// perform the search
-	try {
-		$xs = new XS('books');
-		$search = $xs->search;
-		$search->setCharset('UTF-8');
 
-		if (empty($q)) {
-			// just show hot query
-			$hot = $search->getHotQuery(20, 'currnum');
-		} else {
-			// fuzzy search
-			$search->setFuzzy($m === 'yes');
+// perform the search
+try {
+	$xs_index = $search_type == 'online' ? 'booksonline' : 'books';
+	$xs = new XS($xs_index);
+	$search = $xs->search;
+	$search->setCharset('UTF-8');
 
-			// synonym search
-			$search->setAutoSynonyms($syn === 'yes');
-
-			// set query
-			if (!empty($f) && $f != '_all') {
-				$search->setQuery($f . ':(' . $q . ')');
-			} else {
-				$search->setQuery($q);
-			}
-
-			// set sort
-			if (($pos = strrpos($s, '_')) !== false) {
-				$sf = substr($s, 0, $pos);
-				$st = substr($s, $pos + 1);
-				$search->setSort($sf, $st === 'ASC');
-			}
-
-			// set offset, limit
-			$p = max(1, intval($p));
-			$n = XSSearch::PAGE_SIZE;
-			$search->setLimit($n, ($p - 1) * $n);
-
-			// get the result
-			$search_begin = microtime(true);
-			$docs = $search->search();
-			$search_cost = microtime(true) - $search_begin;
-
-			// get other result
-			$count = $search->getLastCount();
-			$total = $search->getDbTotal();
-
-			if ($xml !== 'yes') {
-				// try to corrected, if resul too few
-				if ($count < 1 || $count < ceil(0.001 * $total)) {
-					$corrected = $search->getCorrectedQuery();
-				}
-				// get related query
-				$related = $search->getRelatedQuery();
-			}
-
-			// gen pager
-			if ($count > $n) {
-				$pb = max($p - 5, 1);
-				$pe = min($pb + 10, ceil($count / $n) + 1);
-				$pager = '';
-				do {
-					$pager .= ($pb == $p) ? '<li class="disabled"><a>' . $p . '</a></li>' : '<li><a href="' . $bu . '&p=' . $pb . '">' . $pb . '</a></li>';
-				} while (++$pb < $pe);
-			}
-		}
-	} catch (XSException $e) {
-		$error = strval($e);
-	}
-
-	// calculate total time cost
-	$total_cost = microtime(true) - $total_begin;
-
-	// XML OUPUT
-	if ($xml === 'yes' && !empty($q)) {
-		header("Content-Type: text/xml; charset=$oe");
-		echo "<?xml version=\"1.0\" encoding=\"$oe\" ?>\n";
-		echo "<xs:result count=\"$count\" total=\"$total\" cost=\"$total_cost\" xmlns:xs=\"http://www.gouyg.com\">\n";
-		if ($error !== '') {
-			echo "  <error><![CDATA[" . $error . "]]></error>\n";
-		}
-		foreach ($docs as $doc) {
-			echo "  <doc index=\"" . $doc->rank() . "\" percent=\"" . $doc->percent() . "%\">\n";
-			foreach ($doc as $k => $v) {
-				echo "    <$k>";
-				echo is_numeric($v) ? $v : "\n      <![CDATA[" . $v . "]]>\n    ";
-				echo "</$k>\n";
-			}
-			echo "  </doc>\n";
-		}
-		echo "</xs:result>\n";
-		exit(0);
-	}
-
-	// output the data
-	if ($i == 1) {
-		$doc = array_pop($docs);
-		$title = $doc ? $doc->title : $q;
-		include dirname(__FILE__) . '/show.tpl';
+	if (empty($q)) {
+		// just show hot query
+		$hot = $search->getHotQuery(20, 'currnum');
 	} else {
-		include dirname(__FILE__) . '/search.tpl';
+		// fuzzy search
+		$search->setFuzzy($m === 'yes');
+
+		// synonym search
+		$search->setAutoSynonyms($syn === 'yes');
+
+		// set query
+		if (!empty($f) && $f != '_all') {
+			$search->setQuery($f . ':(' . $q . ')');
+		} else {
+			$search->setQuery($q);
+		}
+
+		// set sort
+		if (($pos = strrpos($s, '_')) !== false) {
+			$sf = substr($s, 0, $pos);
+			$st = substr($s, $pos + 1);
+			$search->setSort($sf, $st === 'ASC');
+		}
+
+		// set offset, limit
+		$p = max(1, intval($p));
+		$n = XSSearch::PAGE_SIZE;
+		$search->setLimit($n, ($p - 1) * $n);
+
+		// get the result
+		$search_begin = microtime(true);
+		$docs = $search->search();
+		$search_cost = microtime(true) - $search_begin;
+
+		// get other result
+		$count = $search->getLastCount();
+		$total = $search->getDbTotal();
+
+		if ($xml !== 'yes') {
+			// try to corrected, if resul too few
+			if ($count < 1 || $count < ceil(0.001 * $total)) {
+				$corrected = $search->getCorrectedQuery();
+			}
+			// get related query
+			$related = $search->getRelatedQuery();
+		}
+
+		// gen pager
+		if ($count > $n) {
+			$pb = max($p - 5, 1);
+			$pe = min($pb + 10, ceil($count / $n) + 1);
+			$pager = '';
+			do {
+				$pager .= ($pb == $p) ? '<li class="disabled"><a>' . $p . '</a></li>' : '<li><a href="' . $bu . '&p=' . $pb . '">' . $pb . '</a></li>';
+			} while (++$pb < $pe);
+		}
 	}
+} catch (XSException $e) {
+	$error = strval($e);
 }
+
+// calculate total time cost
+$total_cost = microtime(true) - $total_begin;
+
+// XML OUPUT
+if ($xml === 'yes' && !empty($q)) {
+	header("Content-Type: text/xml; charset=$oe");
+	echo "<?xml version=\"1.0\" encoding=\"$oe\" ?>\n";
+	echo "<xs:result count=\"$count\" total=\"$total\" cost=\"$total_cost\" xmlns:xs=\"http://www.gouyg.com\">\n";
+	if ($error !== '') {
+		echo "  <error><![CDATA[" . $error . "]]></error>\n";
+	}
+	foreach ($docs as $doc) {
+		echo "  <doc index=\"" . $doc->rank() . "\" percent=\"" . $doc->percent() . "%\">\n";
+		foreach ($doc as $k => $v) {
+			echo "    <$k>";
+			echo is_numeric($v) ? $v : "\n      <![CDATA[" . $v . "]]>\n    ";
+			echo "</$k>\n";
+		}
+		echo "  </doc>\n";
+	}
+	echo "</xs:result>\n";
+	exit(0);
+}
+
+// output the data
+if ($search_type == 'online') {
+	include dirname(__FILE__) . '/booksonline.tpl';
+}
+elseif ($i == 1) {
+	$doc = array_pop($docs);
+	$title = $doc ? $doc->title : $q;
+	include dirname(__FILE__) . '/show.tpl';
+} else {
+	include dirname(__FILE__) . '/search.tpl';
+}
+
 
