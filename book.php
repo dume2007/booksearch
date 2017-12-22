4706a2ccd02bed4bb9dc0884d7309a590de77170
邓道成 <dengdaocheng@xinhehui.com>
2017-12-22 14:19:36 +0800

    book.tpl

--- book.php
+++ book.php
@@ -8,7 +8,7 @@ $q = $_GET['q'];
 $q = preg_replace('/[^\w]+/i', '', $q);
 $book = $model->find('book', "md5id='{$q}'");
 if(empty($book)) {
-    die('404 Not Found');
+    //die('404 Not Found');
 }
 
 $chapters = $model->query("select * from book_chapter where book_id='".$book['id']."' order by page_id asc,id asc");
